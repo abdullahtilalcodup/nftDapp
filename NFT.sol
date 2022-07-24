f954541097d8e165142231f1d314bb8d49a4d1ad
abdullahTilal <105864450+abdullahtilalcodup@users.noreply.github.com>
2022-07-24 19:41:24 +0500

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -2,10 +2,13 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
 
+  using Strings for uint256;
   string public baseURI;
   string public baseExtension = ".json";
   uint256 public cost = 0.000003 ether;
